d6195ca5f7904d066d49c46797f2b8d82fc3d964
Rasmy-r7 <116500283+Rasmy-r7@users.noreply.github.com>
2023-01-02 13:50:55 +0530

    Add files via upload

---
+++ inuput 2 number add,sub,multi,divi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    float num1,num2;
+    printf("Enter the Number 1 :");
+    scanf("%f",&num1);
+
+    printf("Enter the Number 2 :");
+    scanf("%f",&num2);
+
+    printf("\nAddition       : %f",num1+num2);
+    printf("\nSubtraction    : %f",num1-num2);
+    printf("\nMultiplication : %f",num1*num2);
+    printf("\nDivision       : %f",num1/num2);
+
+
+    return 0;
+}
